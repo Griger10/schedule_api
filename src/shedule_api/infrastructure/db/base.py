f24be3c1e7f80a168623989ae076c98948f10ad6
AJlekCandr-proff` <svznikov@yandex.ru>
2025-07-17 15:45:04 +0300

    style & refactor: Update style .yaml-files.

--- src/shedule_api/infrastructure/db/base.py
+++ src/shedule_api/infrastructure/db/base.py
@@ -8,7 +8,9 @@ class Base(DeclarativeBase):
     __abstract__ = True
 
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
-    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(), onupdate=func.now()
+    )
 
     def __repr__(self):
         return f"<{self.__class__.__name__} {self.__dict__}>"
